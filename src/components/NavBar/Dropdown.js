d92baba4becb890b88d3499f18d2588adbfc6121
Vladyslav K <vladyslav.korotkyy@globallogic.com>
2020-12-16 22:04:43 +0200

    basic universal dropdown

--- src/components/NavBar/Dropdown.js
+++ src/components/NavBar/Dropdown.js
@@ -1,67 +1,25 @@
-import React, { useState } from 'react';
 import './Dropdown.scss';
 
-function Dropdown() {
-  const [closeSub, setSubState] = useState(false);
-  const [closeSubSub, setSubSubState] = useState(false);
-
-  const changeSubState = e => {
-    e.preventDefault();
-    setSubState(!closeSub);
-  };
-
-  const changeSubSubState = e => {
-    e.preventDefault();
-    setSubSubState(!closeSubSub);
-  };
-
-  return (
-    <ul className="sub-menu main">
-      <li>
-        <a href="#">Service1</a>
-      </li>
-      <li>
-        <a href="#">Service2</a>
-      </li>
-      <li className="active-link" id="first-link">
-        <a href="#">Service3</a>
-        <span
-          onClick={changeSubState}
-          className={closeSub ? 'showSubMenu' : 'closeSubMenu'}
-        >
-          <i className="ion-chevron-right" />
-        </span>
-        <ul className="sub-menu">
-          <li>
-            <a href="#">Service1</a>
-          </li>
-          <li>
-            <a href="#">Service2</a>
-          </li>
-          <li className="active-link" id="second-link">
-            <a href="#">Service3</a>
-            <span
-              onClick={changeSubSubState}
-              className={closeSubSub ? 'showSubSubMenu' : 'closeSubSubMenu'}
-            >
+const Dropdown = ({ items: { id, active, options, sub } }) => (
+  <ul key={'ul-' + id} className={`sub-menu ${id === '00' ? 'main' : ''} ${active ? 'active' : ''}`}>
+    {options.map(({ label, isDropdown }, optI) => (
+      isDropdown
+        ? (
+          <li className="option" key={id + optI}>
+            <a href="#">{label}</a>
+            <span>
               <i className="ion-chevron-right" />
             </span>
-            <ul className="sub-menu">
-              <li>
-                <a href="#">Service1</a>
-              </li>
-              <li>
-                <a href="#">Service2</a>
-              </li>
-              <li>
-                <a href="#">Service3</a>
-              </li>
-            </ul>
+            <Dropdown items={sub} />
           </li>
-        </ul>
-      </li>
-    </ul>
-  );
-}
+          )
+        : (
+          <li className="option" key={id + optI}>
+            <a href="#">{label}</a>
+          </li>
+          )
+    ))}
+  </ul>
+);
 
 export default Dropdown;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,79 +1,112 @@
 import './NavBar.scss';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from './Dropdown';
 
-export const NavBar = () => {
-  const [isMenuActive, setMenuState] = useState(false);
-  const [isDropDownActive, setDropDownVisibility] = useState(false);
-
-  const toggleMobileMenu = () => setMenuState(!isMenuActive);
-  const closeMobileMenu = () => setMenuState(false);
-
-  const onMouseEnter = () => {
-    setDropDownVisibility(window.innerWidth >= 960);
-  };
-
-  const onMouseLeave = () => {
-    setDropDownVisibility(false);
-  };
-
-  return (
-    <div className="container">
-      <div className="logo-container">
-        <Link className="navbar-logo">Atomic</Link>
-      </div>
-      <div className="nav-container">
-        <nav className="navbar">
-          <div className="menu-icon" onisMenuActive={toggleMobileMenu}>
-            <i className={isMenuActive ? 'ion-close' : 'ion-android-menu'} />
-          </div>
-          <ul className={isMenuActive ? 'nav-menu active' : 'nav-menu'}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onisMenuActive={closeMobileMenu}>
-                HOME
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                ABOUT
-              </Link>
-            </li>
-            <li
-              className={`nav-item ${setDropDownVisibility ? 'mobileNav' : ''}`}
-              onMouseEnter={onMouseEnter}
-              onMouseLeave={onMouseLeave}
-            >
-              <Link to="/service" className="nav-links">
-                SERVICE
-              </Link>
-              <span onisMenuActive={() => setDropDownVisibility(!isDropDownActive)}>
-                <i
-                  className={isDropDownActive ? 'ion-chevron-up' : 'ion-chevron-down'}
-                />
-              </span>
-              {isDropDownActive && <Dropdown />}
-            </li>
-            <li className="nav-item" onisMenuActive={closeMobileMenu}>
-              <Link to="/" className="nav-links">
-                GALLERY
-              </Link>
-            </li>
-            <li className="nav-item" onisMenuActive={closeMobileMenu}>
-              <Link to="/" className="nav-links">
-                BLOG
-              </Link>
-            </li>
-            <li className="nav-item" onisMenuActive={closeMobileMenu}>
-              <Link to="/" className="nav-links">
-                CONTACT
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+const items = {
+  id: '00',
+  active: true,
+  options: [
+    {
+      label: 'Service1'
+    },
+    {
+      label: 'Service 2'
+    },
+    {
+      label: 'Service 3',
+      isDropdown: true
+    }
+  ],
+  sub: {
+    id: '01',
+    // parentPath: { id: '00', index: '2' },
+    active: false,
+    options: [
+      {
+        label: 'Service1'
+      },
+      {
+        label: 'Service 2'
+      },
+      {
+        label: 'Service 3',
+        isDropdown: true
+      }
+    ],
+    sub: {
+      id: '02',
+      // parentPath: { id: '01', index: '2' },
+      active: false,
+      options: [
+        {
+          label: 'Service1'
+        },
+        {
+          label: 'Service 2'
+        },
+        {
+          label: 'Service 3'
+        }
+      ]
+    }
+  }
 };
+
+const menuItems = [
+  {
+    label: 'HOME'
+  },
+  {
+    label: 'ABOUT'
+  },
+  {
+    label: 'SERVICE',
+    dropdownItems: items
+  },
+  {
+    label: 'GALLERY'
+  },
+  {
+    label: 'CONTACT'
+  }
+];
+
+const NavBar = () => (
+  <div className="container">
+    <div className="logo-container">
+      <Link className="navbar-logo">Atomic</Link>
+    </div>
+    <div className="nav-container">
+      <nav className="navbar">
+        <div className="menu-icon">
+          <i className="ion-close" />
+        </div>
+        <ul className="nav-menu">
+          {menuItems.map(({ label, dropdownItems }) => (
+            dropdownItems
+              ? (
+                <li key={'key-' + label} className="nav-item">
+                  <Link to="/" className="nav-links">
+                    {label}
+                  </Link>
+                  <span>
+                    <i className="ion-chevron-up" />
+                  </span>
+                  <Dropdown items={dropdownItems} />
+                </li>
+                )
+              : (
+                <li key={'key-' + label} className="nav-item">
+                  <Link to="/" className="nav-links">
+                    {label}
+                  </Link>
+                </li>
+                )
+          ))}
+        </ul>
+      </nav>
+    </div>
+  </div>
+);
 
 export default NavBar;
