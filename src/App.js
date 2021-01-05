27dd850f716e02232593a9f5c3664677bb413d5b
Eduard_Kurbatov <62837706+EduardKurbatov@users.noreply.github.com>
2021-01-05 16:41:10 +0200

    feat(app): [WEB-7] created header content, PC & mobile version (#3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
+import HeadingContent from './components/Heading/HeadingContent';
 
 function App() {
   return (
     <Router>
       <NavBar />
+      <HeadingContent />
     </Router>
   );
 }
